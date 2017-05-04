62fd63dca74c72070b2b31dbc2eb002e8f1afe8c
markokajzer <markokajzer91@gmail.com>
2017-05-04 20:08:17 +0900

    Improved usage for main

--- assignment2/main.py
+++ assignment2/main.py
@@ -1,14 +1,14 @@
 # HOW TO USE
 
 # 1. Import a model
-# from model_1 import SampleArchitecture1
+# from model_1 import SampleArchitecture1 as model
 
 # 2. Call the trainer with desired architecture
 # from trainer import process
 # process(SampleArchitecture1.get_model, "/tmp/tf/model1", batch_size=200, steps=500)
 
 
-from model_1 import SampleArchitecture1
+from model_2 import SampleArchitecture2 as model
 from trainer import process
 
-process(SampleArchitecture1.get_model, "/tmp/tf/model1", batch_size=200, steps=500)
+process(model.get_model, "/tmp/tf/modelx", batch_size=200, steps=500)
